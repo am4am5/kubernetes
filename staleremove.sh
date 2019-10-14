994583b0fcb56218a2a184dfb55162c37e979c38
am4am5 <36809996+am4am5@users.noreply.github.com>
2019-10-14 11:54:41 +0300

    Create staleremove.sh

---
+++ staleremove.sh
@@ -0,0 +1,3 @@
+kubectl patch pvc pvc_name -p '{"metadata":{"finalizers":null}}'
+kubectl patch pv pv_name -p '{"metadata":{"finalizers":null}}'
+kubectl patch pod pod_name -p '{"metadata":{"finalizers":null}}'

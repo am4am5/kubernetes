f4ab65f1581b8cb7c8ed81e4681e30de53ebf073
am4am5 <36809996+am4am5@users.noreply.github.com>
2019-10-18 19:00:27 +0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,3 +1,6 @@
 systemctl enable --now kubelet
 
 kubeadm init --pod-network-cidr=10.244.0.0/16
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+

642d582ea35f6e71c6819e23b7e5f97cec66526b
am4am5 <36809996+am4am5@users.noreply.github.com>
2019-10-18 16:28:18 +0300

    Create containerd.sh

---
+++ containerd.sh
@@ -0,0 +1,5 @@
+wget https://storage.googleapis.com/cri-containerd-release/cri-containerd-1.3.0.linux-amd64.tar.gz
+
+tar --no-overwrite-dir -C / -xzf cri-containerd-1.3.0.linux-amd64.tar.gz
+
+rm -rf /opt/containerd

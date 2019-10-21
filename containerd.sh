b4bb1021f904abff39af0aa4aacc10ceffe1fd9a
am4am5 <36809996+am4am5@users.noreply.github.com>
2019-10-21 13:33:45 +0300

    Update containerd.sh

--- containerd.sh
+++ containerd.sh
@@ -11,7 +11,7 @@ nano /etc/containerd/config.toml
       [plugins."io.containerd.grpc.v1.cri".registry.mirrors]
         [plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]
           endpoint = ["https://registry-1.docker.io"]
-        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."kubeworker1:30020"]
-          endpoint = ["http://kubeworker1:30020"]
+        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."172.16.9.49:30020"]
+          endpoint = ["http://172.16.9.49:30020"]
 
 systemctl enable --now containerd

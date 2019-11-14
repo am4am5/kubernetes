649dd9cb1f29c2909410203346c9147a29a2d39c
am4am5 <36809996+am4am5@users.noreply.github.com>
2019-11-14 18:10:15 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,8 +3,10 @@ kubeadm upgrade plan
 kubeadm upgrade apply v1.16.3
 systemctl daemon-reload
 systemctl restart kubelet
+systemctl status kubelet
 
 workers:
 kubeadm upgrade node
 systemctl daemon-reload
 systemctl restart kubelet
+systemctl status kubelet

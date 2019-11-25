c678c89b4246df51de8f9eb518f506eec7b466da
am4am5 <36809996+am4am5@users.noreply.github.com>
2019-11-25 17:18:02 +0300

    Update containerd.sh

--- containerd.sh
+++ containerd.sh
@@ -1,4 +1,4 @@
-wget https://storage.googleapis.com/cri-containerd-release/cri-containerd-1.3.0.linux-amd64.tar.gz
+wget https://storage.googleapis.com/cri-containerd-release/cri-containerd-1.3.1.linux-amd64.tar.gz
 
 tar --no-overwrite-dir -C / -xzf cri-containerd-1.3.0.linux-amd64.tar.gz
 

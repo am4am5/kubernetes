52d87dec9e3160a00de0d3dfb7ea46173d604fab
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-03-17 17:03:57 +0300

    Update rbac.sh

--- rbac.sh
+++ rbac.sh
@@ -1,1 +1,4 @@
-openssl genrsa -out jenkins.key 4096 && openssl req -new -key jenkins.key -out jenkins.csr -subj "/CN=jenkins/O=aismv" && openssl x509 -req -in jenkins.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out jenkins.crt -days 3650
+#openssl genrsa -out jenkins.key 2048 && openssl req -new -key jenkins.key -out jenkins.csr -subj "/CN=jenkins/O=aismv" && openssl x509 -req -in jenkins.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out jenkins.crt -days 3650
+
+kubeadm alpha kubeconfig user --client-name=jenkins --apiserver-advertise-address 172.16.9.47 > jenkins.conf
+kubeadm alpha kubeconfig user --client-name=jenkins --apiserver-advertise-address 172.20.238.229 > jenkins.conf

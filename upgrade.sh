9b01f4223d3191113711e2c3d35210df4c2f425a
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-07-17 13:20:34 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,4 +4,4 @@ kubeadm upgrade apply v1.18.6
 systemctl daemon-reload && systemctl restart kubelet && systemctl status kubelet
 
 workers:
-dnf -y update && kubeadm upgrade node && systemctl daemon-reload && systemctl restart kubelet && systemctl status kubelet
+dnf -y update --nobest && kubeadm upgrade node && systemctl daemon-reload && systemctl restart kubelet && systemctl status kubelet

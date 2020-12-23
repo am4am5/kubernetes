c62ffd5c484ea6b291f7625b987eeda4f733a6d3
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-12-24 00:27:23 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 master:
 kubeadm upgrade plan
-kubeadm upgrade apply v1.20.0
+kubeadm upgrade apply v1.20.1
 systemctl daemon-reload && systemctl restart kubelet && systemctl status kubelet
 
 workers:

162726e5aeb0773d6fb44df36d56468471d94e85
am4am5 <36809996+am4am5@users.noreply.github.com>
2021-02-24 00:24:34 +0300

    Update massdelete.sh

--- massdelete.sh
+++ massdelete.sh
@@ -1,2 +1,2 @@
 $ kubectl get pods -n aismv --no-headers=true | awk '/pattern1|pattern2/{print $1}'| xargs  kubectl delete -n aismv pod
-$ kubectl get services -n aismv --no-headers=true | awk '/aismv-back|aismv-router/aismv-front/{print $1}' | xargs  kubectl delete -n mynamespace service
+$ kubectl get services -n aismv --no-headers=true | awk '/aismv-back|aismv-router|aismv-front/{print $1}' | xargs  kubectl delete -n mynamespace service

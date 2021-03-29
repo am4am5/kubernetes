54f1e8fea779d6fd9123a022afb1a06c306262bd
am4am5 <36809996+am4am5@users.noreply.github.com>
2021-03-30 00:17:02 +0300

    Update containerd.sh

--- containerd.sh
+++ containerd.sh
@@ -1,6 +1,5 @@
 #Install
-wget https://github.com/containerd/containerd/releases/download/v1.4.4/cri-containerd-cni-1.4.4-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-1.4.4-linux-amd64.tar.gz && rm -rf /opt/containerd cri-containerd-cni-1.4.4-linux-amd64.tar.gz && mkdir /etc/containerd && containerd config default > /etc/containerd/config.toml
-/etc/cni/net.d/10-containerd-net.conflist
+wget https://github.com/containerd/containerd/releases/download/v1.4.4/cri-containerd-cni-1.4.4-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-1.4.4-linux-amd64.tar.gz && rm -rf /etc/cni/net.d/10-containerd-net.conflist /opt/containerd cri-containerd-cni-1.4.4-linux-amd64.tar.gz && mkdir /etc/containerd && containerd config default > /etc/containerd/config.toml
 
 nano /etc/containerd/config.toml
 [plugins."io.containerd.grpc.v1.cri".registry]

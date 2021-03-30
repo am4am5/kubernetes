4c7d21c4368f63925912ac6ec60810f76af4dbee
am4am5 <36809996+am4am5@users.noreply.github.com>
2021-03-30 19:29:55 +0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -4,3 +4,6 @@ kubeadm init --pod-network-cidr=10.244.0.0/16
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
+kubeadm token create --print-join-command
+
+kubeadm join 172.16.9.46:6443 --token km97j5.9cdqbfb52nygj3et     --discovery-token-ca-cert-hash sha256:c336361df853fa4f7836d6cbd29bb440d26b3059b733570d9e63be9bb019f622

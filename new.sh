ebf4e0f7f76514026deb067f0f75caac3b47500c
am4am5 <36809996+am4am5@users.noreply.github.com>
2021-04-02 22:38:33 +0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -6,4 +6,4 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documen
 
 kubeadm token create --print-join-command
 
-kubeadm join 172.16.9.46:6443 --token km97j5.9cdqbfb52nygj3et     --discovery-token-ca-cert-hash sha256:c336361df853fa4f7836d6cbd29bb440d26b3059b733570d9e63be9bb019f622
+kubeadm join 172.16.9.46:6443 --token hvq11u.2f5jmhv06ap8b5j2     --discovery-token-ca-cert-hash sha256:c336361df853fa4f7836d6cbd29bb440d26b3059b733570d9e63be9bb019f622

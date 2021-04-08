9ff5cec353b2be95b539a278cab8ded48860735a
am4am5 <36809996+am4am5@users.noreply.github.com>
2021-04-08 18:50:42 +0300

    Update ingresstls.sh

--- ingresstls.sh
+++ ingresstls.sh
@@ -1,3 +1,3 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=EDO/O=IBA"
 
-kubectl create secret tls tls-secret --key tls.key --cert tls.crt
+kubectl -n ingress create secret tls edo-tls --key tls.key --cert tls.crt

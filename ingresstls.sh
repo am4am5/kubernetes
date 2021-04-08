8295edba54c3c2e7a965524b312f3e808e08d688
am4am5 <36809996+am4am5@users.noreply.github.com>
2021-04-08 19:20:23 +0300

    Update ingresstls.sh

--- ingresstls.sh
+++ ingresstls.sh
@@ -1,3 +1,6 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=EDO/O=IBA"
 
 kubectl -n ingress create secret tls edo-tls --key tls.key --cert tls.crt
+
+  annotations:
+    nginx.ingress.kubernetes.io/backend-protocol: "HTTPS"

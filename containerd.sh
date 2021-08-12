9df10eeea2aa87e82a42f78d9c5c0db72b3d070c
am4am5 <36809996+am4am5@users.noreply.github.com>
2021-08-13 00:57:04 +0300

    Update containerd.sh

--- containerd.sh
+++ containerd.sh
@@ -2,23 +2,11 @@
 wget https://github.com/containerd/containerd/releases/download/v1.5.5/cri-containerd-cni-1.5.5-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-1.5.5-linux-amd64.tar.gz && rm -rf /opt/containerd cri-containerd-cni-1.5.5-linux-amd64.tar.gz /etc/cni/net.d/10-containerd-net.conflist && mkdir /etc/containerd && containerd config default > /etc/containerd/config.toml
 
 nano /etc/containerd/config.toml
-[plugins."io.containerd.grpc.v1.cri".registry]
-      [plugins."io.containerd.grpc.v1.cri".registry.mirrors]
-        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]
-          endpoint = ["https://registry-1.docker.io"]
-        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."172.16.9.49:30020"]
-          endpoint = ["http://172.16.9.49:30020"]
-        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.edo.iba"]
-          endpoint = ["https://registry.edo.iba"]
-          
-# X.509 fix 
-        [plugins."io.containerd.grpc.v1.cri".registry.configs]
-          [plugins."io.containerd.grpc.v1.cri".registry.configs."registry.edo.iba".tls]
-            insecure_skip_verify = true
-
-
-[metrics]
-  address = "127.0.0.1:9323"
+    [plugins."io.containerd.grpc.v1.cri".registry]
+      [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry2.edo.iba"]
+        endpoint = ["https://registry2.edo.iba"]
+      [plugins."io.containerd.grpc.v1.cri".registry.configs."registry2.edo.iba".tls]
+        insecure_skip_verify = true
 
 [debug]
   level = "warn"

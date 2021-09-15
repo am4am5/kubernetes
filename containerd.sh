ad672d70d6698b1dd2b3a6ac35cfa0ad0e4dadd6
am4am5 <36809996+am4am5@users.noreply.github.com>
2021-09-15 22:52:24 +0300

    Update containerd.sh

--- containerd.sh
+++ containerd.sh
@@ -1,7 +1,15 @@
 #Install
 wget https://github.com/containerd/containerd/releases/download/v1.5.5/cri-containerd-cni-1.5.5-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-1.5.5-linux-amd64.tar.gz && rm -rf /opt/containerd cri-containerd-cni-1.5.5-linux-amd64.tar.gz /etc/cni/net.d/10-containerd-net.conflist && mkdir /etc/containerd && containerd config default > /etc/containerd/config.toml
 
+tar --no-overwrite-dir -C / -xzf cri-containerd-cni-1.5.5-linux-amd64.tar.gz && rm -rf /opt/containerd cri-containerd-cni-1.5.5-linux-amd64.tar.gz /etc/cni/net.d/10-containerd-net.conflist && mkdir /etc/containerd && containerd config default > /etc/containerd/config.toml
+
 nano /etc/containerd/config.toml
+    [plugins."io.containerd.grpc.v1.cri".registry]
+      [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.gtb.local"]
+        endpoint = ["https://registry.gtb.local"]
+      [plugins."io.containerd.grpc.v1.cri".registry.configs."registry.gtb.local".tls]
+        insecure_skip_verify = true
+
     [plugins."io.containerd.grpc.v1.cri".registry]
       [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry2.edo.iba"]
         endpoint = ["https://registry2.edo.iba"]
@@ -15,6 +23,7 @@ systemctl enable --now containerd && systemctl status containerd
 
 echo "192.168.249.20 registry.gtb.local" >> /etc/hosts
 
+rm -f /etc/cni/net.d/87-podman-bridge.conflist
 
 #Upgrade
 wget https://github.com/containerd/containerd/releases/download/v1.5.5/cri-containerd-cni-1.5.5-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-1.5.5-linux-amd64.tar.gz && rm -rf /opt/containerd /etc/cni/net.d/10-containerd-net.conflist cri-containerd-cni-1.5.5-linux-amd64.tar.gz && systemctl daemon-reload && systemctl restart containerd && systemctl status containerd

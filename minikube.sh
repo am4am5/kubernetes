4bee7ade3c25e8d6bf174174499df8c9f548a2d7
am4am5 <36809996+am4am5@users.noreply.github.com>
2022-06-23 20:09:07 +0300

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -4,10 +4,11 @@ dnf install https://github.com/kubernetes/minikube/releases/download/v1.26.0/min
 
 minikube config --help
 
-minikube config set driver podman && minikube config set container-runtime containerd && minikube config set network-plugin cni && minikube config set enable-default-cni && minikube config set bootstrapper kubeadm && minikube config set memory max && minikube config set cpus max && minikube config set listen-address '0.0.0.0' && minikube config set insecure-registry '10.0.0.0/24'
+minikube config set driver podman && minikube config set container-runtime containerd && minikube config set bootstrapper kubeadm && minikube config set memory max && minikube config set cpus 8 && minikube config set insecure-registry "10.0.0.0/24"
+? minikube config set network-plugin cni minikube config set cni bridge && minikube config set listen-address 0.0.0.0
 minikube config view
 
-minikube start
+minikube start --listen-address=0.0.0.0 --force
 
 #systemd
 cat <<EOF > /usr/lib/systemd/system/minikube.service
@@ -18,10 +19,14 @@ After=network.target local-fs.target containerd.service
 Type=oneshot
 RemainAfterExit=yes
 WorkingDirectory=/root
-ExecStart=/usr/bin/minikube start
+ExecStart=/usr/bin/minikube start --listen-address=0.0.0.0 --force
 ExecStop=/usr/bin/minikube stop
+User=root
+Group=root
 [Install]
 WantedBy=multi-user.target
 EOF
 
 systemctl daemon-reload && systemctl enable --now minikube
+
+minikube status

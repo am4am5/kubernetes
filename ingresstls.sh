4a53780eb4c0ea15ecc7986097dc677b851eef4d
am4am5 <36809996+am4am5@users.noreply.github.com>
2022-07-21 16:56:41 +0300

    Update ingresstls.sh

--- ingresstls.sh
+++ ingresstls.sh
@@ -1,6 +1,34 @@
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=EDO/O=IBA"
+openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:2048 -keyout tls.key -out tls.crt -extfile tls.cfg
 
-kubectl -n ingress create secret tls edo-tls --key tls.key --cert tls.crt
+#nano tls.cfg
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+prompt = no
+
+[req_distinguished_name]
+C = BY
+ST = Minsk
+L = Minsk
+O = IBA
+OU = EDO
+CN = edo.iba
+
+[v3_req]
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = edo.iba
+DNS.2 = *.edo.iba
+
+
+openssl genrsa -out tls.key 2048
+openssl rsa -in tls.key -out tls.crt
+openssl req -new -key tls.crt -out tls.csr
+openssl x509 -req -extensions v3_req -days 3650 -in tls.csr -signkey tls.key -out tls.crt -extfile tls.cfg
+
+
+kubectl -n dev create secret tls edo-tls --key tls.key --cert tls.crt
 
   annotations:
     nginx.ingress.kubernetes.io/backend-protocol: "HTTPS"

e4e3a539ff53023b19c08b2cb8ad48f521b7838b
am4am5 <36809996+am4am5@users.noreply.github.com>
2023-05-13 02:57:35 +0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -2,7 +2,7 @@ systemctl enable --now kubelet
 
 firewall-cmd --add-port=6443 --permanent && firewall-cmd --add-port=10250 --permanent && firewall-cmd --add-port=2379-2380 --permanent && firewall-cmd --reload
 
-kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.92.254 --control-plane-endpoint=nansed
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.92.254 --control-plane-endpoint=192.168.92.254
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 

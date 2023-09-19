153edf6cf54c3382623b5a98e4e236c61c265916
am4am5 <36809996+am4am5@users.noreply.github.com>
2023-09-20 00:57:40 +0300

    Update containerd.sh

--- containerd.sh
+++ containerd.sh
@@ -27,4 +27,4 @@ echo "192.168.249.20 registry.gtb.local" >> /etc/hosts
 rm -f /etc/cni/net.d/87-podman-bridge.conflist
 
 #Upgrade
-wget https://github.com/containerd/containerd/releases/download/v1.6.14/cri-containerd-cni-1.6.14-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-1.6.14-linux-amd64.tar.gz && rm -rf /opt/containerd /etc/cni/net.d/10-containerd-net.conflist cri-containerd-cni-1.6.14-linux-amd64.tar.gz && systemctl daemon-reload && systemctl restart containerd && systemctl status containerd
+CD_VER=1.7.6 && wget https://github.com/containerd/containerd/releases/download/v${CD_VER}/cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && rm -rf /opt/containerd /etc/cni/net.d/10-containerd-net.conflist cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && systemctl daemon-reload && systemctl restart containerd && systemctl status containerd

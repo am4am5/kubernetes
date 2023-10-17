15f638123c369ecd2ef79a2b99beab59ec101de3
am4am5 <36809996+am4am5@users.noreply.github.com>
2023-10-17 17:28:49 +0300

    Update drain.sh

--- drain.sh
+++ drain.sh
@@ -1,1 +1,1 @@
-kubectl drain <node> --delete-emptydir-data=true --grace-period=30 --ignore-daemonsets=true   
+kubectl drain <node> --delete-emptydir-data=true --grace-period=30 --ignore-daemonsets=true --force

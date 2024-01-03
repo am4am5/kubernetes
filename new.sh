7c4b891140768500c33d5547930bf0f8c0693700
am4am5 <36809996+am4am5@users.noreply.github.com>
2024-01-03 20:12:31 +0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,6 +1,9 @@
 systemctl enable --now kubelet
 
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-cert-extra-sans=localhost,127.0.0.1,10.16.64.17
+
 kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.92.254 --control-plane-endpoint=192.168.92.254 --node-name=nansed --apiserver-cert-extra-sans=localhost,127.0.0.1,192.168.92.254,80.94.169.171
+
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 

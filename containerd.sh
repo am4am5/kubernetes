2d483a4a5a64702c12bd25e886df1b9be3be7c2b
am4am5 <36809996+am4am5@users.noreply.github.com>
2025-02-12 22:57:59 +0300

    Update containerd.sh

--- containerd.sh
+++ containerd.sh
@@ -1,55 +1,17 @@
 #Install
-CD_VER=1.7.22 && wget https://github.com/containerd/containerd/releases/download/v${CD_VER}/cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && rm -rf /opt/containerd cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz /etc/cni/net.d/10-containerd-net.conflist && mkdir /etc/containerd && containerd config default > /etc/containerd/config.toml
+CD_VER=1.7.25 && wget https://github.com/containerd/containerd/releases/download/v${CD_VER}/cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && rm -rf /opt/containerd cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz /etc/cni/net.d/10-containerd-net.conflist && mkdir /etc/containerd && containerd config default > /etc/containerd/config.toml
 sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
-
-CD_VER=2.0.2 && wget https://github.com/containerd/containerd/releases/download/v${CD_VER}/containerd-${CD_VER}-linux-amd64.tar.gz && tar --no-overwrite-dir -C /usr/ -xzf containerd-${CD_VER}-linux-amd64.tar.gz && rm -f containerd-${CD_VER}-linux-amd64.tar.gz && mkdir /etc/containerd && containerd config default > /etc/containerd/config.toml
-nano /etc/containerd/config.toml
-[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-  SystemdCgroup = true
-
-nano /usr/lib/systemd/system/containerd.service
-[Unit]
-Description=containerd container runtime
-Documentation=https://containerd.io
-After=network.target
-
-[Service]
-ExecStartPre=-/sbin/modprobe overlay
-ExecStart=/usr/bin/containerd
-Delegate=yes
-KillMode=process
-
-[Install]
-WantedBy=multi-user.target
-
-systemctl daemon-reload
-
-  
-# PAUSE_IMAGE=$(kubeadm config images list | grep pause) && sed -i "s,sandbox_image = .*,sandbox_image = \"$PAUSE_IMAGE\",g" /etc/containerd/config.toml
+PAUSE_IMAGE=$(kubeadm config images list | grep pause) && sed -i "s,sandbox_image = .*,sandbox_image = \"$PAUSE_IMAGE\",g" /etc/containerd/config.toml
 
 nano /etc/containerd/config.toml
-    [plugins."io.containerd.grpc.v1.cri".registry]
-      [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.gtb.local"]
-        endpoint = ["https://registry.gtb.local"]
-      [plugins."io.containerd.grpc.v1.cri".registry.configs."registry.gtb.local".tls]
-        insecure_skip_verify = true
-
-    [plugins."io.containerd.grpc.v1.cri".registry]
+    [plugins.'io.containerd.cri.v1.images'.registry]
       [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.edo.iba"]
         endpoint = ["https://registry.edo.iba"]
       [plugins."io.containerd.grpc.v1.cri".registry.configs."registry.edo.iba".tls]
         insecure_skip_verify = true
-
-[debug]
-  level = "warn"
+      config_path = ''
 
 systemctl enable --now containerd && systemctl status containerd
 
-echo "192.168.249.20 registry.gtb.local" >> /etc/hosts
-
-rm -f /etc/cni/net.d/87-podman-bridge.conflist
-
 #Upgrade
-CD_VER=1.7.22 && wget https://github.com/containerd/containerd/releases/download/v${CD_VER}/cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && rm -rf /opt/containerd /etc/cni/net.d/10-containerd-net.conflist cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && systemctl daemon-reload && systemctl restart containerd && systemctl status containerd
-
-CD_VER=2.0.1 && wget https://github.com/containerd/containerd/releases/download/v${CD_VER}/containerd-${CD_VER}-linux-amd64.tar.gz && tar --no-overwrite-dir -C /usr/ -xzf containerd-${CD_VER}-linux-amd64.tar.gz && rm -f containerd-${CD_VER}-linux-amd64.tar.gz && systemctl daemon-reload && systemctl restart containerd && systemctl status containerd
+CD_VER=1.7.25 && wget https://github.com/containerd/containerd/releases/download/v${CD_VER}/cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && rm -rf /opt/containerd /etc/cni/net.d/10-containerd-net.conflist cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && systemctl daemon-reload && systemctl restart containerd && systemctl status containerd

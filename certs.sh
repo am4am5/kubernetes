eceddab242d9252b2f48dc6fcb9a3198e4df0d90
am4am5 <36809996+am4am5@users.noreply.github.com>
2025-03-27 12:27:28 +0300

    Update certs.sh

--- certs.sh
+++ certs.sh
@@ -6,3 +6,8 @@ kubectl -n kube-system edit cm kubeadm-config
 certificateValidityPeriod: 78840h0m0s
 
 cp /etc/kubernetes/admin.conf ~/.kube/config
+
+nano /etc/yum.repos.d/kubernetes.repo
+dnf --showduplicates list kubeadm
+dnf install kubeadm-<version> kubelet-<version>
+systemctl restart kubelet && systemctl status kubelet

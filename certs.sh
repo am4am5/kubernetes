4a3b86e9ddd672302c1f1e0daa35da7b8eb5f4ea
am4am5 <36809996+am4am5@users.noreply.github.com>
2025-03-27 11:44:56 +0300

    Update certs.sh

--- certs.sh
+++ certs.sh
@@ -1,3 +1,8 @@
 kubeadm certs check-expiration
-kubeadm certs renew
+
+kubeadm certs renew all --cert-duration=78840h0m0s
+
+kubectl -n kube-system edit cm kubeadm-config
+certificateValidityPeriod: 78840h0m0s
+
 cp /etc/kubernetes/admin.conf ~/.kube/config

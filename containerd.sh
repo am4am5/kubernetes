1d7f7f7dd86402ad22e75c61e9fa41f81c6b5d81
am4am5 <36809996+am4am5@users.noreply.github.com>
2026-01-27 14:33:36 +0300

    Update containerd.sh

--- containerd.sh
+++ containerd.sh
@@ -1,7 +1,7 @@
 #Install
 CD_VER=1.7.30 && wget https://github.com/containerd/containerd/releases/download/v${CD_VER}/cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && tar --no-overwrite-dir -C / -xzf cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz && rm -rf /opt/containerd cri-containerd-cni-${CD_VER}-linux-amd64.tar.gz /etc/cni/net.d/10-containerd-net.conflist && mkdir /etc/containerd && containerd config default > /etc/containerd/config.toml
 sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
-PAUSE_IMAGE=$(kubeadm config images list | grep pause) && sed -i "s,sandbox_image = .*,sandbox_image = \"$PAUSE_IMAGE\",g" /etc/containerd/config.toml
+PAUSE_IMAGE=$(kubeadm config images list | grep pause) && sed -i "s,sandbox_image = .*,sandbox_image = \"$PAUSE_IMAGE\",g" /etc/containerd/config.toml && systemctl restart containerd && systemctl status containerd
 
 nano /etc/containerd/config.toml
     [plugins.'io.containerd.cri.v1.images'.registry]
